19bde249ad30484b5316c9bb7dd09bd96758b6b7
Darya Kolenchenko <daryakolenchenko@Daryas-MacBook-Pro.local>
2024-11-11 11:49:20 +0100

    feat: pass 44 tests

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -562,8 +562,12 @@ function propagateItemsByPositionIndex(/* arr */) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  if (n < 0) {
+    const start = n * -1;
+    return arr.slice(start).concat(arr.slice(0, start));
+  }
+  return arr.slice(n + 1).concat(arr.slice(0, n + 1));
 }
 
 /**
